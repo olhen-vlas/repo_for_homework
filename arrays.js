dab824b6f93dd89514c28d7394d743acf719bea1
Olha <olya.shutileva@gmail.com>
2026-05-12 00:48:22 +0300

    added homework_4

---
+++ arrays.js
@@ -0,0 +1,27 @@
+const stringArray = ["Apple", "Banana", "Cherry"];
+const numberArray = [1, 2, 3, 4, 5];
+const booleanArray = [true, false, true];
+const anyArray = ["Hello", 42, true, { key: "value" }];
+
+console.log(stringArray);
+console.log(numberArray);
+console.log(booleanArray);
+console.log(anyArray);
+
+stringArray.push("Date");
+console.log(stringArray);
+
+numberArray.pop();
+console.log(numberArray);
+
+anyArray.splice(1, 1, "World");
+console.log(anyArray);
+
+console.log('--- forEach loop ---');
+booleanArray.forEach((item, index) => {
+    console.log(`${index}: ${item}`);
+});
+
+console.log('--- map result ---');
+const doubledNumbers = numberArray.map(num => num * 2);
+console.log(doubledNumbers);
---
+++ objects.js
@@ -0,0 +1,19 @@
+const complexObject = {
+    name: "John Doe",
+    age: 30,
+    address: {
+        city: "Kyiv",
+        street: "Khreshchatyk",
+        zipCode: 1001
+    },
+    hobbies: ["Photography", "Hiking", "Coding"],
+    displayInfo() {
+        console.log(`User: ${this.name}, Age: ${this.age}`);
+        console.log(`Lives in: ${this.address.city}, ${this.address.street}`);
+        console.log(`Hobbies: ${this.hobbies.join(", ")}`);
+    }
+};
+
+const executeDisplay = (obj) => obj.displayInfo();
+
+executeDisplay(complexObject);
